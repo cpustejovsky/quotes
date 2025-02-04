5a2559db392b1e6cbf1af0050a5881ee8102d32c
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-02-04 10:33:26 -0500

    feat: add prayer if prayer arg is present

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"log"
 	"math/rand/v2"
+	"os"
 	"os/exec"
 
 	"github.com/cpustejovsky/quotes/converter"
@@ -13,6 +14,16 @@ import (
 var quotes string
 
 func main() {
+	for _, arg := range os.Args {
+		if arg == "prayer" {
+			cmd := exec.Command("notify-send", "Be the helper of my soul, O god, for I walk in the midst of many snares. Deliver me from them and save me, for You are good, and You love mankind.")
+			_, err := cmd.Output()
+			if err != nil {
+				log.Fatal(err)
+			}
+			os.Exit(0)
+		}
+	}
 	quotes, err := converter.ConvertQuotes(quotes)
 	if err != nil {
 		log.Fatal(err)
